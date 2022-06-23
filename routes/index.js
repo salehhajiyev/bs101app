ea7b6a01b243a196a8552c370d1c5aaa3fa6647d
Saleh Hajiyev <Salehhajiyev@b3-0.bs101.local>
2022-06-23 09:58:14 +0400

    shekiller elave edildi

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res) {
-    res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Express', "data": getData() });
 });
+var getData = function () {
+    var data = {
+        'item1': 'https://images.unsplash.com/photo-1563422156298-c778a278f9a5',
+        'item2': 'https://images.unsplash.com/photo-1620173834206-c029bf322dba',
+        'item3': 'https://images.unsplash.com/photo-1602491673980-73aa38de027a'
+    }
+    return data;
+}
 
 module.exports = router;
